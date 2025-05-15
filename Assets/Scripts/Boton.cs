d32ea831da2fcf6c575b47bf59caddcbb13cfbed
santi901 <49379041@est.ort.edu.ar>
2025-05-15 12:08:29 -0300

    Clonador

--- Assets/Scripts/Boton.cs
+++ Assets/Scripts/Boton.cs
@@ -2,25 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClonaCubos : MonoBehaviour
+public class Boton : MonoBehaviour
 {
-    public GameObject CuboPrefab;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void ClonarCubo()
-    {
-        Instantiate(CuboPrefab);
-
     }
 }
